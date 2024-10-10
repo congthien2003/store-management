84a688dd2908e335e48ecadc3a372b796145e41d
MNhatdayy <nhatnm2003@gmail.com>
2024-10-10 08:48:17 +0700

    update-mystore-logout

--- client/src/app/@admin/admin.component.ts
+++ client/src/app/@admin/admin.component.ts
@@ -1,56 +1,65 @@
-import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ActivatedRoute, Router, RouterOutlet } from "@angular/router";
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { AuthenticationService } from '../core/services/auth/authentication.service';
 
 @Component({
-	selector: "app-admin",
-	standalone: true,
-	imports: [CommonModule, RouterOutlet],
-	templateUrl: "./admin.component.html",
-	styleUrls: ["./admin.component.scss"],
+  selector: 'app-admin',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
-	currentURL: string = "dashboard";
-	activeIndex: number = 0;
+  currentURL: string = 'dashboard';
+  activeIndex: number = 0;
 
-	constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
 
-	swithRoute(index: number) {
-		this.activeIndex = index;
-		switch (index) {
-			case 0: {
-				this.router.navigate(["/admin/dashboard"]);
-				break;
-			}
-			case 1: {
-				this.router.navigate(["/admin/user"]);
-				break;
-			}
-			case 2: {
-				this.router.navigate(["/admin/store"]);
-				break;
-			}
-		}
-	}
+  swithRoute(index: number) {
+    this.activeIndex = index;
+    switch (index) {
+      case 0: {
+        this.router.navigate(['/admin/dashboard']);
+        break;
+      }
+      case 1: {
+        this.router.navigate(['/admin/user']);
+        break;
+      }
+      case 2: {
+        this.router.navigate(['/admin/store']);
+        break;
+      }
+      case 3: {
+        this.authService.logout();
+        this.router.navigate(['/auth/login']);
+        break;
+      }
+    }
+  }
 
-	bindActiveMenu(url: string) {
-		switch (url) {
-			case "/pages": {
-				this.activeIndex = 0;
-				break;
-			}
-			case "/pages/create": {
-				this.activeIndex = 1;
-				break;
-			}
-			case "/pages/find": {
-				this.activeIndex = 2;
-				break;
-			}
-			default: {
-				this.activeIndex = -1;
-				break;
-			}
-		}
-	}
+  bindActiveMenu(url: string) {
+    switch (url) {
+      case '/pages': {
+        this.activeIndex = 0;
+        break;
+      }
+      case '/pages/create': {
+        this.activeIndex = 1;
+        break;
+      }
+      case '/pages/find': {
+        this.activeIndex = 2;
+        break;
+      }
+      default: {
+        this.activeIndex = -1;
+        break;
+      }
+    }
+  }
 }
