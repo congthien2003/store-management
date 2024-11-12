325ac9c675478d9487973cede91fa6009be83a94
Cong Thien <conggthien17@gmail.com>
2024-11-12 16:16:11 +0700

    FE_Add_Chart_Staff_Module

---
+++ client/src/app/@staff/components/invoice/invoice.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-invoice',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './invoice.component.html',
+  styleUrls: ['./invoice.component.scss']
+})
+export class InvoiceComponent {
+
+}
