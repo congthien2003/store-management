e0040b7426215891b2a81458715d1054b63b5720
MNhatdayy <nhatnm2003@gmail.com>
2024-11-20 20:57:43 +0700

    commit-to-pull

---
+++ core/StoreManagement.Application/DTOs/Response/FoodBestSellerResponse.cs
@@ -0,0 +1,15 @@
+﻿using StoreManagement.Application.DTOs.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManagement.Application.DTOs.Response
+{
+    public class FoodBestSellerResponse
+    {
+        public List<FoodDTO> ListFood { get; set; }
+        public List<FoodDTO> Top4BestSeller { get; set; }
+    }
+}
