785ced7020306b8b1068d04c552cb8a7eb00d8bc
MNhatdayy <nhatnm2003@gmail.com>
2024-12-13 11:15:26 +0700

    commit-resolve-code

--- client/src/app/@owner/owner-routing.module.ts
+++ client/src/app/@owner/owner-routing.module.ts
@@ -14,14 +14,73 @@ import { ownerGuard } from '../@auth/guards/owner.guard';
 import { InforUserComponent } from './components/management/infor-user/infor-user.component';
 import { ChangePasswordComponent } from '../@auth/components';
 import { MyTicketComponent } from './components/management/my-ticket/my-ticket.component';
-import { PredictRevenueComponent } from "./components/management/predict-revenue/predict-revenue.component";
-import { ComboComponent } from "./components/management/combo/combo.component";
+import { PredictRevenueComponent } from './components/management/predict-revenue/predict-revenue.component';
+import { ComboComponent } from './components/management/combo/combo.component';
 
 const routes: Routes = [
- 			{
-        		path: 'my-ticket',
-        		component: MyTicketComponent,
-      		},
+  {
+    path: '',
+    component: OwnerComponent,
+    canActivate: [ownerGuard],
+    children: [
+      {
+        path: 'dashboard',
+        component: OwnerDashboardComponent,
+      },
+      {
+        path: 'my-store',
+        component: MyStoreComponent,
+      },
+      {
+        path: 'table',
+        component: TableComponent,
+      },
+      {
+        path: 'category',
+        component: CategoryComponent,
+      },
+      {
+        path: 'food',
+        component: FoodComponent,
+      },
+      {
+        path: 'order',
+        component: OrderComponent,
+      },
+      {
+        path: 'invoice',
+        component: InvoiceComponent,
+      },
+      {
+        path: 'staff',
+        component: StaffComponent,
+      },
+      {
+        path: 'analytics',
+        component: AnalyticsComponent,
+      },
+      {
+        path: 'infor-user',
+        component: InforUserComponent,
+      },
+      {
+        path: 'change-password',
+        component: ChangePasswordComponent,
+      },
+      {
+        path: 'predict-revenue',
+        component: PredictRevenueComponent,
+      },
+      {
+        path: 'combos',
+        component: ComboComponent,
+      },
+      {
+        path: 'my-ticket',
+        component: MyTicketComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
